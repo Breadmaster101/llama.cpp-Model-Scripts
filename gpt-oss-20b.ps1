d1fb1cb6a4d5f0a96963d0e5ed34a89bf9502bfd
breadmaster <92588686+Breadmaster101@users.noreply.github.com>
2025-10-30 16:58:20 -0700

    set top k to 100 to improve generation speeds

--- gpt-oss-20b.ps1
+++ gpt-oss-20b.ps1
@@ -1,3 +1,3 @@
 winget update llama.cpp
 Start "http://127.0.0.1:8080/"
-llama-server -hf unsloth/gpt-oss-20b-GGUF:UD-Q4_K_XL --jinja -ngl 99 --threads -1 --ctx-size 8192 --temp 1.0 --top-p 1.0 --top-k 0 -ot ".ffn_.*_exps.=CPU" -fa on --cache-type-k q8_0 --cache-type-v q8_0
+llama-server -hf unsloth/gpt-oss-20b-GGUF:UD-Q4_K_XL --jinja -ngl 99 --threads -1 --ctx-size 8192 --temp 1.0 --top-p 1.0 --top-k 100 -ot ".ffn_.*_exps.=CPU" -fa on --cache-type-k q8_0 --cache-type-v q8_0
